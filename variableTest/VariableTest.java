2dd35f5b4c45f59538f3187326cf5928e12b964e
aidenlee <lih9490@gmail.com>
2024-06-09 02:28:11 +0900

    feat(): variable_study(with integer and long)

---
+++ variableTest/VariableTest.java
@@ -0,0 +1,12 @@
+package variableTest;
+
+public class VariableTest {
+    public static void main(String[] args){
+        byte bnum = -127;
+        System.out.println(bnum);
+
+//        int num = 12345676700;
+        long lNum = 123445556600l;
+        System.out.println(lNum);
+    }
+}
